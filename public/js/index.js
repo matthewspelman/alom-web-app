5bd7c3cf3d4fdbc97ccbd10c5ebb3d3f49b6d2b3
Matthew Spelman <matthewspelman@gmail.com>
2018-04-28 16:02:27 -0400

    More required changes to index.html core page.
    improvements to photos and video.

--- public/js/index.js
+++ public/js/index.js
@@ -17,7 +17,7 @@ function myFunction() {
     }
 }
 
-// ===== Scroll to Top ==== //
+// ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
@@ -25,7 +25,6 @@ $(window).scroll(function() {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
     }
 });
-
 $('#return-to-top').click(function() {      // When arrow is clicked
     $('body,html').animate({
         scrollTop : 0                       // Scroll to top of body
